7a127f0255aa6546f6d30a617adbbaf4443decdd
GeonSang Lee <116617354+geonsangg@users.noreply.github.com>
2023-04-08 14:51:43 +0900

    uploaded at 2023. 4. 8. 오후 2:51:44

---
+++ programmers/없는 숫자 더하기/solution.java
@@ -0,0 +1,19 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/86051
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int[] arr = {0,1,2,3,4,5,6,7,8,9};
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<numbers.length; j++) {
+                if(arr[i] == numbers[j]) {
+                    arr[i] = 0;
+                }
+            }
+        }
+        for(int s : arr) {
+            answer += s;
+        }
+        return answer;
+    }
+}
