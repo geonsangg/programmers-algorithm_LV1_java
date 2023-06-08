9b703776c8d8b30c0601f590f5a3b4ed1e7dbb5a
GeonSang Lee <116617354+geonsangg@users.noreply.github.com>
2023-06-08 23:58:32 +0900

    uploaded at 2023. 6. 8. 오후 11:58:44

---
+++ programmers/최대공약수와 최소공배수/solution.java
@@ -0,0 +1,16 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12940
+
+class Solution {
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        int minNum = (n < m ? n : m);
+        for(int i= minNum; i>0; i--) {
+            if(n % i == 0 && m  % i == 0) {
+                answer[0] = i;
+                break;
+            }
+        }
+        answer[1] = n*m/answer[0];
+        return answer;
+    }
+}
